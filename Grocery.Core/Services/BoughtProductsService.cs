9028e04b28c7b4fb869a6b76fb1c60f2a4082bf5
jonawoning <jonawoningyt@gmail.com>
2025-10-02 11:35:25 +0200

    Styling toevoegen en objects toevoegen i.p.v. vars. (HBO ICT Code Richtlijnen)
    - Styling toevoegen aan BougtProductsView.
    - Objects goed defineren i.p.v. vars.

--- Grocery.Core/Services/BoughtProductsService.cs
+++ Grocery.Core/Services/BoughtProductsService.cs
@@ -15,19 +15,18 @@ namespace Grocery.Core.Services
         public List<BoughtProducts> Get(int productId)
         {
             var groceryListItems = groceryListItemsRepository.GetByProductId(productId);
-            var product = productRepository.Get(productId);
+            
+            Product? product = productRepository.Get(productId);
+            if (product == null) return new List<BoughtProducts>();
 
-            if (product == null)
-                return new List<BoughtProducts>();
+            List<BoughtProducts> result = new List<BoughtProducts>();
 
-            var result = new List<BoughtProducts>();
-
-            foreach (var groceryListItem in groceryListItems)
+            foreach (GroceryListItem groceryListItem in groceryListItems)
             {
-                var groceryList = groceryListRepository.Get(groceryListItem.GroceryListId);
+                GroceryList ?groceryList = groceryListRepository.Get(groceryListItem.GroceryListId);
                 if (groceryList == null) continue;
 
-                var client = clientRepository.Get(groceryList.ClientId);
+                Client ?client = clientRepository.Get(groceryList.ClientId);
                 if (client == null) continue;
 
                 result.Add(new BoughtProducts(client, groceryList, product));
